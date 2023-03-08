35a9f5c96f7963857ab6423e0c089bbd28f1eb97
Khoi Nguyen Nguyen Tran <khoinguyen09092004@gmail.com>
2023-03-08 07:03:02 -0700

    "Menu Implementation"

--- src/Assignment8.java
+++ src/Assignment8.java
@@ -12,25 +12,89 @@ import java.util.ArrayList;
 public class Assignment8 {
 
 	public static void main(String[] args) {
-		// menu
+		// Menu Options
+		char inputOpt = ' ';
+		String inputLine;
+		double sumA; 
+		int sumB, numB1, numB2, numC;
+		String resultC, inputD1, inputD2, resultD;
 		try {
+			printMenu();
+			InputStreamReader isr = new InputStreamReader(System.in);
+			BufferedReader stdin = new BufferedReader(isr);
+			do {
+				inputLine = stdin.readLine().trim();
+				if (inputLine.isEmpty()) {
+					continue;
+				}
+				inputOpt = inputLine.charAt(0);
+				inputOpt = Character.toUpperCase(inputOpt);
 
+				switch(inputOpt) {
+					case 'A': //Calculate the sum of all elements in an array of doubles
+						System.out.print("Enter numbers (0 to finish): ");
+						double[] listToCalc = parseDoubles(stdin);
+						sumA = CalcSumArray(listToCalc);
+						System.out.print("Sum of elements in array: " + sumA + "\n");
+						break;
+					
+					case 'B': //Calculate the sum of all integers between two numbers (including the two numbers)
+						System.out.print("Enter the first number: ");
+						numB1 = readInteger(stdin);
+						numB2 = readInteger(stdin);
+						sumB = CalcSumAtoB(numB1, numB2);
+						System.out.print("The sum of all integers between " + numB1 + " and " + numB2 + " is: " + sumB + "\n");
+						break;
 
+					case 'C':
+						System.out.print("Enter an integer to factorize: ");
+						numC = readInteger(stdin);
+						resultC = NumAFactorize(numC);
+						System.out.print("The prime factorization of " + numC + " is: " + resultC + "\n");
+						break;
+
+					case 'D':
+						System.out.print( "Please enter string: ");
+						inputD1 = stdin.readLine().trim();
+						System.out.print("Please enter substring to remove: ");
+						inputD2 = stdin.readLine().trim();
+						resultD = RemoveSubStr(inputD1, inputD2);
+						break;
+
+					default:
+						System.out.print("Please choose a character between A and E.\n");
+						break;
+
+				}
+
+			} while (inputOpt != 'E' || inputLine.length() != 1);
 		} catch (IOException ex) {
-			
+			System.out.print("IO Exception");
 		}
 	}
 
 	// A: recursive method that calculates the sum of all elements in an array of
 	// doubles and returns the sum
+	public static double CalcSumArray(double[] list) {
+		
+	}
+
 
 	// B: recursive method that calculates the sum of all integers between two
 	// numbers (including the two numbers) and returns the sum
+	public static int CalcSumAtoB(int one, int two) {
+
+	}
 
 	// C: recursive method that calculates the prime factorization of an integer and returns a string as a result
+	public static String NumAFactorize(int one) {
+
+	}
 
 	// D: recursive method that removes all occurrences of a specified substring in a string and returns the result string
+	public static String RemoveSubStr(String remove, String needToRemove) {
 
+	}
 
 	// ----------------------------------------------------------------------------------------
 
